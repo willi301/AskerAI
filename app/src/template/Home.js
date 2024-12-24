03980988d903c3789d425638b74fed2e9acf56a1
willi301 <will301will@gmail.comm>
2024-12-23 23:14:04 -0800

    added routing

---
+++ app/src/template/Home.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+
+
+function Home() {
+  return (
+    <h1>Hello, world</h1>
+  );
+}
+
+export default Home;
